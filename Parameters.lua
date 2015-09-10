aa7dc7a51d5c477ecd96fcfc0c9499eb7c267851
Clement Farabet <cfarabet@twitter.com>
2015-09-10 18:04:02 -0400

    Added initializers to Parameters().

--- Parameters.lua
+++ Parameters.lua
@@ -6,6 +6,14 @@ function Parameters:__init(...)
    self.gradWeight = torch.Tensor(...)
 end
 
+function Parameters:normal(...)
+   self.weight:normal(...)
+end
+
+function Parameters:uniform(...)
+   self.weight:uniform(...)
+end
+
 function Parameters:updateOutput()
    self.output = self.weight
    return self.output
