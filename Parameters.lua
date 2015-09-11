52a217ec69c7285e73edba000533b9662ead210d
Clement Farabet <cfarabet@twitter.com>
2015-09-11 11:10:51 -0400

    Added Jacobian tests + fixed Parameters.

--- Parameters.lua
+++ Parameters.lua
@@ -4,6 +4,11 @@ function Parameters:__init(...)
    parent.__init(self)
    self.weight = torch.Tensor(...)
    self.gradWeight = torch.Tensor(...)
+   self:reset()
+end
+
+function Parameters:reset()
+   self:normal(0,0.01)
 end
 
 function Parameters:normal(...)
@@ -22,4 +27,8 @@ end
 function Parameters:accGradParameters(_, gradOutput, scale)
    scale = scale or 1
    self.gradWeight:add(scale, gradOutput)
+end
+
+function Parameters:zeroGradParameters()
+   self.gradWeight:zero()
 end
--- test.lua
+++ test.lua
@@ -1,9 +1,18 @@
 -- Pkg to test
 local nnop = require 'nnop'
 local nngraph = require 'nngraph'
+nngraph.setDebug(true)
 
+-- Tester:
 local totem = require 'totem'
 local tester = totem.Tester()
+
+-- For Jacobian tests:
+local nn = require 'nn'
+local Jacobian = nn.Jacobian
+
+-- Precision for Jacobian tests:
+local precision = 1e-5
 
 -- Tests:
 local tests = {
@@ -16,7 +25,7 @@ local tests = {
       tester:assert(linearWeight:forward():dim(), 2, 'incorrect nb of dims')
    end,
 
-   Linear = function()
+   LinearBasic = function()
       -- Create parameter:
       local linearWeight = nnop.Parameters(10,100)
       local linearBias = nnop.Parameters(10)
@@ -28,8 +37,8 @@ local tests = {
       local res = linear:forward({torch.randn(100), linearWeight:forward(), linearBias:forward()})
 
       -- Test:
-      tester:eq(res:dim(), 1, 'incorrect nb of dims')
-      tester:eq(res:size(1), 10, 'incorrect size')
+      tester:asserteq(res:dim(), 1, 'incorrect nb of dims')
+      tester:asserteq(res:size(1), 10, 'incorrect size')
 
       -- Backward:
       local grads = linear:backward({torch.randn(100), linearWeight:forward(), linearBias:forward()}, res)
@@ -42,11 +51,11 @@ local tests = {
       local res = linear:forward({torch.randn(100), parameters.weight:forward(), parameters.bias:forward()})
 
       -- Test:
-      tester:eq(res:dim(), 1, 'incorrect nb of dims')
-      tester:eq(res:size(1), 10, 'incorrect size')
+      tester:asserteq(res:dim(), 1, 'incorrect nb of dims')
+      tester:asserteq(res:size(1), 10, 'incorrect size')
    end,
 
-   nngraph = function()
+   LinearGraph = function()
       -- create base modules:
       local linear1 = nnop.Linear(10,100)
       local tanh1 = nn.Tanh()
@@ -61,17 +70,58 @@ local tests = {
       -- build final model:
       local model = nn.gModule({input}, {output})
 
-      nngraph.setDebug(true)
-
+      -- geometry tests:
       local input = torch.rand(10)
       local gradOutput = torch.rand(2)
       local output = model:forward(input)
       local gradInput = model:updateGradInput(input, gradOutput)
       model:accGradParameters(input, gradOutput)
 
-      tester:eq(output:dim(), 1 , 'incorrect nb of dims')
-      tester:eq(output:size(1), 2, 'incorrect output size')
-      tester:eq(gradInput:size(), input:size(), 'gradInput wrong size')
+      tester:asserteq(output:dim(), 1 , 'incorrect nb of dims')
+      tester:asserteq(output:size(1), 2, 'incorrect output size')
+      tester:asserteq(gradInput:size(1), input:size(1), 'gradInput wrong size')
+
+      -- look at parameters:
+      local parameters,gradParameters = model:parameters()
+      tester:asserteq(#parameters, 4, 'incorrect nb of parameters')
+      tester:asserteq(#parameters, #gradParameters , 'incorrect nb of gradients')
+
+      -- test zeroing:
+      for i,gradParams in ipairs(gradParameters) do
+         gradParams:fill(1)
+      end
+      model:zeroGradParameters()
+      for i,gradParams in ipairs(gradParameters) do
+         tester:asserteq(gradParams:sum(), 0, 'error on zeroGradParameters()')
+      end
+   end,
+
+   LinearJacobian = function()
+      -- create base modules:
+      local linear1 = nnop.Linear(10,100)
+      local tanh1 = nn.Tanh()
+      local linear2 = nnop.Linear(100,2)
+
+      -- bind them in a graph:
+      local input = nn.Identity()()
+      local layer1 = linear1({input, linear1.parameterNodes.weight(), linear1.parameterNodes.bias()})
+      local layer2 = tanh1(layer1)
+      local output = linear2({layer2, linear2.parameterNodes.weight(), linear2.parameterNodes.bias()})
+
+      -- build final model:
+      local model = nn.gModule({input}, {output})
+
+      -- Test backprop:
+      local input = torch.Tensor(10):zero()
+      local err = Jacobian.testJacobian(model, input)
+      tester:assertlt(err, precision, 'error on gradInput')
+
+      -- Test internal weights:
+      local parameters,gradParameters = model:parameters()
+      for i = 1,#parameters do
+         local err = Jacobian.testJacobianParameters(model, input, parameters[1], gradParameters[1])
+         tester:assertlt(err, precision, 'error on gradParameters['..i..']')
+      end
    end
 }
 
